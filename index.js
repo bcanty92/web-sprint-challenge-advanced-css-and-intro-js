edb1a62f4cf965ce134f4f56fe912361cfe435be
Brittany Canty <bcanty92@gmail.comgit config --global user.email:bcanty92@gmail.com>
2020-07-04 03:40:55 -0500

    add

--- index.js
+++ index.js
@@ -228,20 +228,25 @@ console.log(artists[8]);
  * For example, if getArtistByIndex is invoked with the artists dataset and the number 0,
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
-function getArtistByIndex(array,index) {
- 
+function getArtistByIndex(name ,number){
+  artistsArray = 'Edouard Manet' ;
+  index = 7;
 
-  return 'The artist at index'+ index+ array[index]
+  return `The artist at index ${index} is ${artistsArray}`;
 }
+console.log(artistByIndex());
 
 
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/* Code here */){
-
-  /* Code here */
-
+function get20s(artistData){
+  data = [name, years]
+  for (let i = 0; i == 1800-1900; i++)
+  return(get20s());
 }
+  console.log(get20s());
+
+
 
 
 /* Task 5: Create a function called `removeArtist` that takes two arguments:
